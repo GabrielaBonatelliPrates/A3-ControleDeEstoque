6921e5c475b32f3bb765cf8014a6b24caa3c2f99
Lais Portela <laispaivaportela@outlook.com>
2025-05-13 16:16:57 -0300

    adiciona imports importantes

--- src/main/java/model/ControleEstoque.java
+++ src/main/java/model/ControleEstoque.java
@@ -1,7 +1,16 @@
 package model;
+
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.List;
 
 public class ControleEstoque {
    private final List<Produto>produtos = new ArrayList<>();//cria lista que vai armazenar os produtos
+   private final Scanner input = new Scanner(System.in); //cria scanner para a classe toda
+
+
+
+
    
-}
+
+
