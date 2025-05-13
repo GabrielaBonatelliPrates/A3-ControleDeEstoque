c05ed650527c33c81d1238f4b4468c53491d6880
GabrielaBonatelliPrates <gabriela.bonatelli06@gmail.com>
2025-05-13 16:24:19 -0300

    Adiciona método "CadastraProduto"

--- src/main/java/model/ControleEstoque.java
+++ src/main/java/model/ControleEstoque.java
@@ -5,12 +5,13 @@ import java.util.Scanner;
 import java.util.List;
 
 public class ControleEstoque {
-   private final List<Produto>produtos = new ArrayList<>();//cria lista que vai armazenar os produtos
-   private final Scanner input = new Scanner(System.in); //cria scanner para a classe toda
 
+    private final List<Produto> produtos = new ArrayList<>();//cria lista que vai armazenar os produtos
+    private final Scanner input = new Scanner(System.in); //cria scanner para a classe toda
 
-
-
-   
-
-
+    public void cadastraProduto() {
+        Produto produto = new Produto(); //instanciar a classe produto (cria um objeto "produto")
+        produto.lerDados();
+        produtos.add(produto);
+    }
+}
