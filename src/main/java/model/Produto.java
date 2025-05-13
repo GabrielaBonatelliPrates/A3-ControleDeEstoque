937784d5e84ebe07b40d751a4ce398d8049b7d80
Estermrn <darosaester97@gmail.com>
2025-05-13 15:57:10 -0300

    Adiciona a classe Produto

--- src/main/java/model/Produto.java
+++ src/main/java/model/Produto.java
@@ -1,5 +1,97 @@
 package model;
 
+import java.util.Scanner;
+
 public class Produto {
-    
+
+    Scanner sc = new Scanner(System.in);
+
+    //Declaração dos atributos privados da classe Produto
+    private String nomeProduto;
+    private double precoUnit;
+    private int unidadeProduto;
+    private int quantEstoque;
+    private int quantMin;
+    private int quantMax;
+    private Categoria categoria;
+
+    public Produto() {
+    }
+
+    public Produto(String nomeProduto, double precoUnit, int unidadeProduto, int quantEstoque, int quantMin, int quantMax, Categoria categoria) {
+        this.nomeProduto = nomeProduto;
+        this.precoUnit = precoUnit;
+        this.unidadeProduto = unidadeProduto;
+        this.quantEstoque = quantEstoque;
+        this.quantMin = quantMin;
+        this.quantMax = quantMax;
+        this.categoria = categoria;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public double getPrecoUnit() {
+        return precoUnit;
+    }
+
+    public void setPrecoUnit(double precoUnit) {
+        this.precoUnit = precoUnit;
+    }
+
+    public int getUnidadeProduto() {
+        return unidadeProduto;
+    }
+
+    public void setUnidadeProduto(int unidadeProduto) {
+        this.unidadeProduto = unidadeProduto;
+    }
+
+    public int getQuantEstoque() {
+        return quantEstoque;
+    }
+
+    public void setQuantEstoque(int quantEstoque) {
+        this.quantEstoque = quantEstoque;
+    }
+
+    public int getQuantMin() {
+        return quantMin;
+    }
+
+    public void setQuantMin(int quantMin) {
+        this.quantMin = quantMin;
+    }
+
+    public int getQuantMax() {
+        return quantMax;
+    }
+
+    public void setQuantMax(int quantMax) {
+        this.quantMax = quantMax;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public void leituraProduto() {
+        this.nomeProduto = sc.nextLine();
+        this.precoUnit = sc.nextDouble();
+        this.unidadeProduto = sc.nextInt();
+        this.quantEstoque = sc.nextInt();
+        this.quantMin = sc.nextInt();
+        this.quantMax = sc.nextInt();
+        //this.categoria = sc.nextLine();
+
+    }
 }
