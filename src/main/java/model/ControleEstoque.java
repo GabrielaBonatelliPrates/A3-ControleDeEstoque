280e9dfef17a88aa5d6018f72cef23a7abc69e3c
Lais Portela <laispaivaportela@outlook.com>
2025-05-13 15:22:47 -0300

    adiciona a lista que vai armazenar os produtos

--- src/main/java/model/ControleEstoque.java
+++ src/main/java/model/ControleEstoque.java
@@ -2,5 +2,6 @@ package model;
 import java.util.Scanner;
 
 public class ControleEstoque {
+   private final List<Produto>produtos = new ArrayList<>();//cria lista que vai armazenar os produtos
    
 }
