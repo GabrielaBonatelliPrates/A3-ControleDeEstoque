87abf4e716081448508b57ee00227e7693fb3a87
Estermrn <darosaester97@gmail.com>
2025-05-15 15:49:11 -0300

    Atualização em "Pegando o tamanho" e adição de método que limpa a quebra de linha

--- src/main/java/model/Categoria.java
+++ src/main/java/model/Categoria.java
@@ -18,7 +18,7 @@ public class Categoria {
         this.tamanho = tamanho;
         this.embalagem = embalagem;
     }
-    
+
     public String getNome() {
         return nome;
     }
@@ -44,32 +44,32 @@ public class Categoria {
     }
 
     public void lerDados() {
-        
-        
+
         this.nome = sc.nextLine(); //Pegando o nome
 
         //Pegando o tamanho
         System.out.print("Insira o tamanho (P/M/G)");
         char resposta = Character.toUpperCase(sc.next().charAt(0));
-        if (resposta == 'P'){
-            this.tamanho = 'P';
-        }if (resposta == 'M'){
-            this.tamanho = sc.next().charAt(0);
+        if (resposta == 'P' || resposta == 'M' || resposta == 'G') {
+            this.tamanho = resposta;
         } else {
             this.tamanho = sc.next().charAt(0);
         }
-        
+
+        sc.nextLine(); //Limpa a quebra de linha que sobrou
+
         //Pegando a embalagem
-        System.out.print("Insira a embalagem (Vidro/Plastico/Metal)");
+        System.out.print("Insira a embalagem (Vidro/Plastico/Metal): ");
         String embalagens = sc.nextLine();
-        if (embalagens == "Vidro"){
+
+        if (embalagens.equalsIgnoreCase("Vidro")) {
             this.embalagem = "Vidro";
-        }if (embalagens == "Plastico"){
+        } else if (embalagens.equalsIgnoreCase("Plastico")) {
             this.embalagem = "Plastico";
         } else {
             this.embalagem = "Metal";
         }
-        
+
         //Verifiquem se ta certo, caso estiver errado algo favor avisar e arrumar
     }
 }
