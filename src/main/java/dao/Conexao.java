72b5c99dc6920c1d0b34b476bc2d5ae25dc4eb6d
Mateo Padilla <mateo.padilla210@gmail.com>
2025-05-26 20:07:19 -0300

    feat: adicionando classe "Conexao" no pacote "dao" fix: corrigindo versão de conetor em "pom"

---
+++ src/main/java/dao/Conexao.java
@@ -0,0 +1,16 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/controle_estoque";
+    private static final String USUARIO = "root";
+    private static final String SENHA = "2007";
+
+    public static Connection conectar() throws SQLException {
+        return DriverManager.getConnection(URL, USUARIO, SENHA);
+    }
+}
