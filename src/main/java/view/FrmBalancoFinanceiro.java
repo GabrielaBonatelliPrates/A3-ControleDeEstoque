0ded16b49b634af7b8f4b37a9b59c7b054ac8c9a
Mateo <158166080+Mateo-Padilla@users.noreply.github.com>
2025-06-02 10:28:51 -0300

    fix: corrigindo JFrame "FrmBalancoFinanceiro"

--- src/main/java/view/FrmBalancoFinanceiro.java
+++ src/main/java/view/FrmBalancoFinanceiro.java
@@ -96,13 +96,13 @@ public class FrmBalancoFinanceiro extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(BalancoFinanceiro.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmBalancoFinanceiro.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(BalancoFinanceiro.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmBalancoFinanceiro.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(BalancoFinanceiro.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmBalancoFinanceiro.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(BalancoFinanceiro.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmBalancoFinanceiro.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
         //</editor-fold>
@@ -110,7 +110,7 @@ public class FrmBalancoFinanceiro extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new BalancoFinanceiro().setVisible(true);
+                new FrmBalancoFinanceiro().setVisible(true);
             }
         });
     }
