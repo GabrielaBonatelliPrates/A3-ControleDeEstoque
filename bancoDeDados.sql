c138402d64cbc8c4eb2ec276a255ea51d63c1d7d
Mateo <158166080+Mateo-Padilla@users.noreply.github.com>
2025-06-08 23:03:17 -0300

    Update bancoDeDados.sql

--- bancoDeDados.sql
+++ bancoDeDados.sql
@@ -10,7 +10,9 @@ CREATE TABLE IF NOT EXISTS produtos (
     estoque_atual INT NOT NULL,
     estoque_minimo INT NOT NULL,
     estoque_maximo INT NOT NULL,
-    idCategoria INT NOT NULL
+    nome_categoria VARCHAR(100) NOT NULL,
+    tamanho VARCHAR(50) NOT NULL,
+    embalagem VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS categorias (
@@ -20,31 +22,11 @@ CREATE TABLE IF NOT EXISTS categorias (
     embalagem VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS movimentacao (
-	idMovimentacao INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    tipoMovimentacao VARCHAR (50) NOT NULL
-    );
-
-CREATE TABLE IF NOT EXISTS produtoXmovimentacao(
-	PRIMARY KEY (IdProduto, idMovimentacao),
-	dataMovimentacao date NOT NULL,
-    quantidadeMovimentada INT NOT NULL,
+CREATE TABLE IF NOT EXISTS movimentacao_estoque (
+    idMovimentacao INT AUTO_INCREMENT PRIMARY KEY,
+    tipoMovimentacao VARCHAR(20) NOT NULL,
     idProduto INT NOT NULL,
-    idMovimentacao INT NOT NULL
-    );
-
-ALTER TABLE produtoXmovimentacao
-ADD FOREIGN KEY (idProduto) REFERENCES produtos (idProduto),
-ADD FOREIGN KEY (idMovimentacao) REFERENCES movimentacao (idMovimentacao);
-
-			
-ALTER TABLE produtos
-ADD FOREIGN KEY (idCategoria) REFERENCES categorias (idCategoria);
-
-SELECT p.idCategoria AS idCategoria,
-	   c.nomeCategoria AS nomeCategoria,
-COUNT(p.idProduto) AS total_produtos
-FROM produtos p, categorias c 
-WHERE p.idCategoria = c.idCategoria
-GROUP BY p.idCategoria;
-
+    quantidadeMovimentada INT NOT NULL,
+    dataMovimentacao DATE NOT NULL,
+    FOREIGN KEY (idProduto) REFERENCES produtos(idProduto)
+);
