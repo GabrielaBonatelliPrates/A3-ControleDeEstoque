eb7b953dbed9b330aeb968fcfd8e3740fcb96e4a
GabrielaBonatelliPrates <gabriela.bonatelli06@gmail.com>
2025-06-10 11:51:15 -0300

    docs: adicionando documentação "Produto"

--- src/main/java/model/Produto.java
+++ src/main/java/model/Produto.java
@@ -1,10 +1,14 @@
 package model;
 
+/**classe Produto é uma classe para instanciar produtos
+ *
+ * @author Estermrn
+ */
 public class Produto {
 
-
-
-    //variaveis de intancia
+    /**
+     * declaração das variáveis de intancia
+     */
     private String nomeProduto;
     private int idProduto;
     private double precoUnit;
@@ -18,11 +22,23 @@ public class Produto {
     private double valorTotal = precoUnit * quantidadeEstoque;
     private double estoqueTotal;
 
-
-    //construtores
+    /**
+     * construtor da classe Produto
+     */
     public Produto() {
     }
 
+    /**
+     * construtor da classe Produto
+     * @param nomeProduto valor inicial para o nome do produto
+     * @param idProduto valor inicial para o id do produto
+     * @param precoUnit valor inicial para o preço unitáriodo produto
+     * @param unidadeProduto valor inicial para a unidade do produto
+     * @param quantidadeEstoque valor inicial para a quantidade de produto em estoque
+     * @param estoqueMinimo valor inicial para o estoque mínimo de produto
+     * @param estoqueMaximo valor inicial para o estoque máximo de produto
+     * @param categoria
+     */
     public Produto(String nomeProduto, int idProduto, double precoUnit, String unidadeProduto, int quantidadeEstoque, int estoqueMinimo, int estoqueMaximo, Categoria nomeCategoria) {
         this.nomeProduto = nomeProduto;
         this.idProduto = idProduto;
@@ -34,6 +50,16 @@ public class Produto {
         this.categoria = categoria;
     }
 
+    /**
+     *
+     * @param nomeProduto
+     * @param precoUnit
+     * @param unidadeProduto
+     * @param quantidadeEstoque
+     * @param estoqueMinimo
+     * @param estoqueMaximo
+     * @param categoria
+     */
     public Produto(String nomeProduto, double precoUnit, String unidadeProduto, int quantidadeEstoque, int estoqueMinimo, int estoqueMaximo, Categoria categoria) {
         this.nomeProduto = nomeProduto;
         this.precoUnit = precoUnit;
@@ -45,103 +71,202 @@ public class Produto {
     }
 
     //getters e setters
+
+    /**
+     *
+     * @return nome do procuto
+     */
     public String getNomeProduto() {
         return nomeProduto;
     }
 
+    /**
+     *
+     * @param nomeProduto atribui um nome para o produto
+     */
     public void setNomeProduto(String nomeProduto) {
         this.nomeProduto = nomeProduto;
     }
 
+    /**
+     *
+     * @return preço unitario do produto
+     */
     public double getPrecoUnit() {
         return precoUnit;
     }
 
+    /**
+     *
+     * @param precoUnit atribui um preço unitário para o produto
+     */
     public void setPrecoUnit(double precoUnit) {
         this.precoUnit = precoUnit;
     }
 
+    /**
+     *
+     * @return unidade do produto
+     */
     public String getUnidadeProduto() {
         return unidadeProduto;
     }
 
+    /**
+     *
+     * @param unidadeProduto atribui uma unidade para o produto
+     */
     public void setUnidadeProduto(String unidadeProduto) {
         this.unidadeProduto = unidadeProduto;
     }
 
+    /**
+     *
+     * @return quantidade do produto em estoque
+     */
     public int getQuantidadeEstoque() {
         return quantidadeEstoque;
     }
 
+    /**
+     *
+     * @param quantidadeEstoque atribui uma quatidade em estoque para o produto
+     */
     public void setQuantidadeEstoque(int quantidadeEstoque) {
         this.quantidadeEstoque = quantidadeEstoque;
     }
 
+    /**
+     *
+     * @return estoque mínimo 
+     */
     public int getEstoqueMinimo() {
         return estoqueMinimo;
     }
 
+    /**
+     *
+     * @param estoqueMinimo atribui um estoque mínimo para o produto
+     */
     public void setEstoqueMinimo(int estoqueMinimo) {
         this.estoqueMinimo = estoqueMinimo;
     }
 
+    /**
+     *
+     * @return estoque máximo
+     */
     public int getEstoqueMaximo() {
         return estoqueMaximo;
     }
 
+    /**
+     *
+     * @param estoqueMaximo atribui um estoque máximo para o produto
+     */
     public void setEstoqueMaximo(int estoqueMaximo) {
         this.estoqueMaximo = estoqueMaximo;
     }
 
+    /**
+     *
+     * @return 
+     */
     public Categoria getCategoria() {
         return categoria;
     }
 
+    /**
+     *
+     * @param categoria
+     */
     public void setCategoria(Categoria categoria) {
         this.categoria = categoria;
     }
 
+    /**
+     *
+     * @return id do produto
+     */
     public int getIdProduto() {
         return idProduto;
     }
 
+    /**
+     *
+     * @param idProduto atribui um id para o produto
+     */
     public void setIdProduto(int idProduto) {
         this.idProduto = idProduto;
     }
 
+    /**
+     *
+     * @return
+     */
     public boolean isAcimaMedia() {
         return acimaMedia;
     }
 
+    /**
+     *
+     * @param acimaMedia
+     */
     public void setAcimaMedia(boolean acimaMedia) {
         this.acimaMedia = acimaMedia;
     }
 
+    /**
+     *
+     * @return
+     */
     public boolean isAbaixoMedia() {
         return abaixoMedia;
     }
 
+    /**
+     *
+     * @param abaixoMedia
+     */
     public void setAbaixoMedia(boolean abaixoMedia) {
         this.abaixoMedia = abaixoMedia;
     }
 
+    /**
+     *
+     * @return
+     */
     public double getValorTotal() {
         return valorTotal;
     }
 
+    /**
+     *
+     * @param valorTotal
+     */
     public void setValorTotal(double valorTotal) {
         this.valorTotal = valorTotal;
     }
 
+    /**
+     *
+     * @return
+     */
     public double getEstoqueTotal() {
         return estoqueTotal;
     }
 
+    /**
+     *
+     * @param estoqueTotal
+     */
     public void setEstoqueTotal(double estoqueTotal) {
         this.estoqueTotal += valorTotal;
     }
     
-    
+    /**
+     *
+     */
     public void verificaMediaAcima() {
         if (getQuantidadeEstoque() >= getEstoqueMaximo()) {
             setAcimaMedia(true);
@@ -150,6 +275,9 @@ public class Produto {
         }
     }
 
+    /**
+     *
+     */
     public void verificaMediaAbaixo() {
         if (getQuantidadeEstoque() <= getEstoqueMinimo()) {
             setAbaixoMedia(true);
