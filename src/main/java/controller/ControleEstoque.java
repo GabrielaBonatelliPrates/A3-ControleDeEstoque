94ac423e94a061be899207d3e7fa77a6d32000de
Mateo <158166080+Mateo-Padilla@users.noreply.github.com>
2025-06-10 13:41:34 -0300

    feat: adicionando documentação de classe "ControleEstoque".

--- src/main/java/controller/ControleEstoque.java
+++ src/main/java/controller/ControleEstoque.java
@@ -6,13 +6,27 @@ import dao.CategoriaDAO;
 import dao.MovimentacaoDAO;
 import view.FrmMenuPrincipal;
 
+/**classe ControleEstoque é uma classe utilizada para gerenciar a movimentação do estoque
+ *
+ * @author laispaivaportela
+ */
 public class ControleEstoque {
-    //instancia os DAO
+    
+
+    /**metodos de instancia para efetivar o controle do estoque
+     *
+     * @param produtoDAO instancia um novo produto do banco de dados
+     * @param categoriaDAO instancia uma nova categoria do banco de dados
+     * @param movimentacaoDAO instancia uma nova movimentação do banco de dados
+     */
     protected CategoriaDAO categoriaDAO = new CategoriaDAO();
-     protected ProdutoDAO produtoDAO = new ProdutoDAO(categoriaDAO);
+    protected ProdutoDAO produtoDAO = new ProdutoDAO(categoriaDAO);
     protected MovimentacaoDAO movimentacaoDAO = new MovimentacaoDAO(produtoDAO);
-         
-    //inicia a aplicação
+
+    /**
+     * 
+     *@param iniciaAplicação metodo que inicia o sistema
+     */
     public void iniciaAplicação(){
         //instancia a interface gráfica do menu principal
          FrmMenuPrincipal telaMenu = new FrmMenuPrincipal(produtoDAO, categoriaDAO, movimentacaoDAO);
